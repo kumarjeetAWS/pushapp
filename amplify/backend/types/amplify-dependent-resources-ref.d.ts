3deef9c1d412d77842896cc41b824f052b8451d7
kumarjeetAWS <daskumr@amazon.com>
2024-10-15 11:56:49 +0530

    feat: final one

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "pushapp": {
+    "finalapp": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
